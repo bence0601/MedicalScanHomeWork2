8af30bd534a5d8df84e9b0a5d8667cb3801e52ff
Bence Nagy <nagybence0601@gmail.com>
2024-03-15 15:48:50 +0100

    Add : Added CSS and HTML to the jitsi component's corresonding files

--- Jitsi-Angular/src/app/jitsi/jitsi.component.ts
+++ Jitsi-Angular/src/app/jitsi/jitsi.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit,AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import {JitsiService} from './../services/jitsi.service';
 
 
 @Component({
@@ -8,6 +11,23 @@ import { Component, OnInit,AfterViewInit } from '@angular/core';
   templateUrl: './jitsi.component.html',
   styleUrl: './jitsi.component.css'
 })
-export class JitsiComponent {
+export class JitsiComponent implements OnInit {
+  constructor(private router: Router, private jitsiService: JitsiService) {}
+  ngOnInit(): void {
+    this.jitsiService.moveRoom(this.jitsiService.namePrincipalRoom, true);
+  }
+
+  executeCommand(data) {
+    console.log(
+      'this.jitsiService.getParticipants():',
+      this.jitsiService.getParticipants()
+    );
+
+    this.jitsiService.api.executeCommand(
+      'sendEndpointTextMessage',
+      this.jitsiService.getParticipants(),
+      'mover a principal'
+    );
+  }
 
 }
