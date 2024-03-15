666b28c3be7f62f9c55abee7e2c5afa0f797049e
Bence Nagy <nagybence0601@gmail.com>
2024-03-15 16:01:29 +0100

    Add : added Jitsi Script into index.html

--- Jitsi-Angular/src/app/app.routes.ts
+++ Jitsi-Angular/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { JitsiComponent } from './jitsi/jitsi.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ThankYouComponent,
+  },
+  {
+    path: 'jitsi',
+    component: JitsiComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
