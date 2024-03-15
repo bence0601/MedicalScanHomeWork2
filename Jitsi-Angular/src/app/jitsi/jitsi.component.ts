a0ac395c69a791c852b38ee29a6decf681fb1cab
Bence Nagy <nagybence0601@gmail.com>
2024-03-15 15:43:49 +0100

    Add : project folder, jitsi component

---
+++ Jitsi-Angular/src/app/jitsi/jitsi.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit,AfterViewInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-jitsi',
+  standalone: true,
+  imports: [],
+  templateUrl: './jitsi.component.html',
+  styleUrl: './jitsi.component.css'
+})
+export class JitsiComponent {
+
+}
